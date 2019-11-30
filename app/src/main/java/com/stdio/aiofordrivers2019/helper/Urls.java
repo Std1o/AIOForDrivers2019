aa8413a3b559a7d217cea8f0bb4c4b1cceb6e785
Std1o <kwork-stdio@mail.ru>
2019-11-30 14:20:12 +0300

    0.0.7

--- app/src/main/java/com/stdio/aiofordrivers2019/helper/Urls.java
+++ app/src/main/java/com/stdio/aiofordrivers2019/helper/Urls.java
@@ -9,11 +9,11 @@ public class Urls {
     public static final String REGISTER_APPLY_URL =  "/appDriver/driverRegister.php";
     public static final String START_INFO_URL =  "/appDriver/takeDriverStartInfo.php";
     public static final String SEND_COORS_URL =  "/appDriver/driverCoords.php";
-    public static final String GET_FREE_ORDERS_URL =  "/taxi/appDriver/freeOrdersList.php";
-    public static final String TAKE_ORDER_URL =  "/taxi/appDriver/takeOrderByDriver.php";
-    public static final String TAKEN_ORDER_LIST_URL =  "/taxi/appDriver/takenOrdersList.php";
-    public static final String UPLOAD_FOTO_URL =  "/taxi/appDriver/uploadDriverFoto.php";
-    public static final String INFO_OLD_ORDERS_URL =  "/taxi/appDriver/infoOldOrders.php";
-    public static final String INFO_PAYMENT_URL =  "/taxi/appDriver/infoPayments.php";
+    public static final String GET_FREE_ORDERS_URL =  "/appDriver/freeOrdersList.php";
+    public static final String TAKE_ORDER_URL =  "/appDriver/takeOrderByDriver.php";
+    public static final String TAKEN_ORDER_LIST_URL =  "/appDriver/takenOrdersList.php";
+    public static final String UPLOAD_FOTO_URL =  "/appDriver/uploadDriverFoto.php";
+    public static final String INFO_OLD_ORDERS_URL =  "/appDriver/infoOldOrders.php";
+    public static final String INFO_PAYMENT_URL =  "/appDriver/infoPayments.php";
 }
 
