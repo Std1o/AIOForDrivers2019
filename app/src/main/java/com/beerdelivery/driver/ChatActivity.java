57f91852114bbdb9402f0f814b39b06e328a324e
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-07 20:02:49 +0300

    0.6.7

--- app/src/main/java/com/beerdelivery/driver/ChatActivity.java
+++ app/src/main/java/com/beerdelivery/driver/ChatActivity.java
@@ -5,22 +5,30 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.beerdelivery.driver.adapter.ChatAdapter;
 import com.beerdelivery.driver.model.ChatMessageModel;
 
 import java.util.ArrayList;
 
-public class ChatActivity extends AppCompatActivity {
+public class ChatActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ArrayList<ChatMessageModel> dataList;
     private RecyclerView rv;
+    ImageView sendButton, ivClose;
+    EditText messageEditText;
+    ChatAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         initRecyclerView();
+        initViews();
+        setListeners();
     }
 
     private void initRecyclerView() {
@@ -31,6 +39,17 @@ public class ChatActivity extends AppCompatActivity {
         initializeAdapter();
     }
 
+    private void initViews() {
+        sendButton = findViewById(R.id.sendButton);
+        ivClose = findViewById(R.id.ivClose);
+        messageEditText = findViewById(R.id.messageEditText);
+    }
+
+    private void setListeners() {
+        sendButton.setOnClickListener(this);
+        ivClose.setOnClickListener(this);
+    }
+
     private void initializeData(){
         dataList = new ArrayList<>();
         dataList.add(new ChatMessageModel("Сообщение 1", "Stdio"));
@@ -38,8 +57,20 @@ public class ChatActivity extends AppCompatActivity {
         dataList.add(new ChatMessageModel("Сообщение 3", "Stdio"));
     }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sendButton:
+                dataList.add(new ChatMessageModel(messageEditText.getText().toString(), "Stdio"));
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.ivClose:
+                finish();
+                break;
+        }
+    }
+
     private void initializeAdapter(){
-        ChatAdapter adapter = new ChatAdapter(dataList, this);
+        adapter = new ChatAdapter(dataList, this);
         rv.setAdapter(adapter);
     }
 }
